4606edc857418d5c021cfc1f837d75ab70b8fe8b
Lucas Drummond <ldrummond@wustl.edu>
2019-04-24 22:23:05 -0500

    Pushing dist

--- dist/js/chunk-2d2102cc.34146ea2.js
+++ dist/js/chunk-2d2102cc.34146ea2.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2102cc"],{b789:function(t,c,n){"use strict";n.r(c);var s=function(){var t=this,c=t.$createElement,n=t._self._c||c;return n("div",{staticClass:"cart"},[t._v("This is a cart")])},a=[],e={},i=e,l=n("2877"),r=Object(l["a"])(i,s,a,!1,null,null,null);c["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d2102cc.5afc65e4.js.map
+//# sourceMappingURL=chunk-2d2102cc.34146ea2.js.map
